346529eddcca5d791baa6f005fd1716317c9e136
kostabkpark <kostabkpark@gmail.com>
2025-06-26 09:38:10 +0900

    ec2-docker install user data

--- ec2.tf
+++ ec2.tf
@@ -5,17 +5,14 @@ resource "aws_instance" "sample-ec2-01" {
   associate_public_ip_address = true # 퍼블릭 IP 자동 할당
   subnet_id = aws_subnet.sample-subnet-public-01.id
   vpc_security_group_ids = [aws_security_group.sample-sg-elb.id]
-  user_data = <<EOF
-		#! /bin/bash
-    while fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1; do
-      sleep 1
-    done
-    apt update
-    apt install -y nginx
-    systemctl start nginx
-    systemctl enable nginx
-    echo "This is Test Server------1" > /var/www/html/index.nginx-debian.html
-  EOF
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update
+              sudo apt install -y docker.io
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              sudo usermod -aG docker ubuntu
+              EOF
   root_block_device {
     volume_size = "30"
     volume_type = "gp3"
@@ -35,17 +32,14 @@ resource "aws_instance" "sample-ec2-02" {
   associate_public_ip_address = true # 퍼블릭 IP 자동 할당
   subnet_id = aws_subnet.sample-subnet-public-02.id
   vpc_security_group_ids = [aws_security_group.sample-sg-elb.id]
-  user_data = <<EOF
-		#! /bin/bash
-    while fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1; do
-      sleep 1
-    done
-    apt update
-    apt install -y nginx
-    systemctl start nginx
-    systemctl enable nginx
-    echo "This is Test Server-------2" > /var/www/html/index.nginx-debian.html
-  EOF
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update
+              sudo apt install -y docker.io
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              sudo usermod -aG docker ubuntu
+              EOF
   root_block_device {
     volume_size = "30"
     volume_type = "gp3"
